df691e71699ad10cff5cad81879b99dd3ea1e7e6
henrique <henrique-lourenco@hotmail.com>
2023-03-23 11:12:24 -0300

    segunda aula sobre palavras reservadas.

--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -2,8 +2,23 @@ public class MinhaClasse {
     
     public static void main(String[] args) {
         
-    System.out.print ("Olá Turma, sejam bem-vindos");
+    System.Out.print ("Olá Turma, sejam bem-vindos");
+/*        
+    //exemplos de erros de digitação comuns
 
-    }
+    int numero&um = 1; //Os únicos símbolos permitidos são _ e $.
+    int 1numero = 1; //Uma variável não pode começar com númericos.
+    int numero um = 1 // Não pode ter espaços no nome da variável.
+    int long = 1 // long faz parte das palavras reservadas da linguagem, não podendo assim ser usada como variável. 
+    
+//Declaração válida de variáveis
+
+int numero$um = 1;
+int numero1 = 1;
+int numeroum = 1;
+int longo = 1;
+ */
+    
+}
 
 }
