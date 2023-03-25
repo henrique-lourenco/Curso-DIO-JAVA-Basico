2330fc9f68b76f215db8e92c28096062bc954d25
henrique <henrique-lourenco@hotmail.com>
2023-03-25 15:55:37 -0300

    atualização

--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -1,24 +1,25 @@
 public class MinhaClasse {
-    
+
     public static void main(String[] args) {
-        
-    System.Out.print ("Olá Turma, sejam bem-vindos");
-/*        
-    //exemplos de erros de digitação comuns
 
-    int numero&um = 1; //Os únicos símbolos permitidos são _ e $.
-    int 1numero = 1; //Uma variável não pode começar com númericos.
-    int numero um = 1 // Não pode ter espaços no nome da variável.
-    int long = 1 // long faz parte das palavras reservadas da linguagem, não podendo assim ser usada como variável. 
-    
-//Declaração válida de variáveis
+        System.out.println("Olá Turma, sejam bem-vindos");
+        /*
+         * //exemplos de erros de digitação comuns
+         * 
+         * int numero&um = 1; //Os únicos símbolos permitidos são _ e $.
+         * int 1numero = 1; //Uma variável não pode começar com númericos.
+         * int numero um = 1 // Não pode ter espaços no nome da variável.
+         * int long = 1 // long faz parte das palavras reservadas da linguagem, não
+         * podendo assim ser usada como variável.
+         * 
+         * //Declaração válida de variáveis
+         * 
+         * int numero$um = 1;
+         * int numero1 = 1;
+         * int numeroum = 1;
+         * int longo = 1;
+         */
 
-int numero$um = 1;
-int numero1 = 1;
-int numeroum = 1;
-int longo = 1;
- */
-     
-}
+    }
 
 }
